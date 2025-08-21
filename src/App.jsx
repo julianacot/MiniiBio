0d0b1442cac3b491b0a662df5e949da8bcb6e8e0
julianacot <132670022+julianacot@users.noreply.github.com>
2025-08-21 21:27:56 +0000

    pronto

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center" }}>       
+
+      <h1 style={{ fontFamily: "Comic Sans", color: "green", fontWeight: "bold" }}>
+      MINI BIO
+      </h1>
+      
+      <img src="/public/WhatsAppImage.jpeg" height={600} width={500} />
+      <p style= {{ fontFamily: "Comic Sans", fontSize: "16pt", fontWeight: "bold"}}> Juliana Tenório
+        <br></br>
+        Aluna do curso Ciências da Computação
+        <br></br>
+        Universidade Católica de Pernambuco
+      </p>
+
+
+    
+    </div>
+  )
+}
